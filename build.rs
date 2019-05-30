dac584ae6aa164240880cc2b20369ea56cd9e60e
Dave Townsend <dtownsend@oxymoronical.com>
2019-05-29 19:29:44 -0700

    Fix linux build failure.

--- build.rs
+++ build.rs
@@ -4,10 +4,12 @@ fn main() {
     cc::Build::new()
         .file("cityhash_1/city.cc")
         .include("cityhash_1")
+        .cpp(true)
         .compile("cityhash_1");
 
     cc::Build::new()
         .file("cityhash_1_1_1/city.cc")
         .include("cityhash_1_1_1")
+        .cpp(true)
         .compile("cityhash_1_1_1");
 }
